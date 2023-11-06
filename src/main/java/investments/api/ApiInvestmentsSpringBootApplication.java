e404d684cf80dee3c0685e7da562bfe625499bc4
T1091996 Rickson Araujo Da Silva <t1091996@interno.bb.com.br>
2023-11-06 11:09:25 -0300

    initializing the project

---
+++ src/main/java/investments/api/ApiInvestmentsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package investments.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInvestmentsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInvestmentsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/investments/api/ApiInvestmentsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package investments.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInvestmentsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
