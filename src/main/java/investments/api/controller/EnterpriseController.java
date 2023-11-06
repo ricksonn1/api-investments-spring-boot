e4ffc0880da6a9c84bd2a319da682ba1db60d53f
T1091996 Rickson Araujo Da Silva <t1091996@interno.bb.com.br>
2023-11-06 13:28:27 -0300

    create all endpoints of entity Enterprise

--- src/main/java/investments/api/controller/EnterpriseController.java
+++ src/main/java/investments/api/controller/EnterpriseController.java
@@ -3,14 +3,16 @@ package investments.api.controller;
 import investments.api.domain.Enterprise;
 import investments.api.dto.EnterpriseDTO;
 import investments.api.repository.EnterpriseRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("enterprises")
@@ -29,4 +31,40 @@ public class EnterpriseController {
 
     }
 
+    @GetMapping
+    public ResponseEntity<Page<Enterprise>> getAllEnterprises(Pageable pageable) {
+        Page<Enterprise> enterprises = repository.findAll(pageable);
+        return ResponseEntity.ok(enterprises);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOneEnterprise(@PathVariable Long id) {
+
+        var enterpriseById = repository.findById(id);
+        return ResponseEntity.ok(enterpriseById);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity updateEnterprise(@PathVariable Long id, @RequestBody @Valid EnterpriseDTO data) {
+        Optional<Enterprise> optionalEnterprise = repository.findById(id);
+        if (optionalEnterprise.isPresent()) {
+            Enterprise enterprise = optionalEnterprise.get();
+            enterprise.setName(data.name());
+            enterprise.setTicker(data.ticker());
+            enterprise.setSector(data.sector());
+            return ResponseEntity.ok().build();
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deletEnterprise(@PathVariable Long id) {
+
+        var delet = repository.findById(id);
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
